a4275d3ab217ecf36df511de60d3a90c1bafcb51
kanako <budoukan0424@gmail.com>
2021-02-12 22:12:19 +0900

    practiced cloudstore(fetching and writing data), but I have a error of 'map'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
+import React, {useState} from 'react';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
 import logo from './logo.svg';
 import './App.css';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAhBE0EfVjaL4NypnSJnOwLr9CneSpZfiY",
+  authDomain: "plusma-1927f.firebaseapp.com",
+  projectId: "plusma-1927f",
+  storageBucket: "plusma-1927f.appspot.com",
+  messagingSenderId: "929406529962",
+  appId: "1:929406529962:web:5992c2c773ad8b672c7bd3",
+  measurementId: "G-DSN26WMK72"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 function App() {
+  const {users, setUsers} = useState([]);
+  const handleFetchButton = async () => {
+    const db = firebase.firestore();
+    const snapshot = await db
+      .collection('users')
+      .get();
+    const _users = [];
+    snapshot.forEach(doc => {
+      _users.push({
+        userId: doc.id,
+        ...doc.data()
+      });
+    });
+
+  setUsers(_users);
+};
+
+const userListItems = users.map(user => {
+  return (
+    <li key={user.userId}>{user.name} : {user.location} : {user.age}</li>
+  );
+});
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World!</h1>
+      <button onClick={handleFetchButton}>取得</button>
+      <ul>{userListItems}</ul>
     </div>
   );
 }
