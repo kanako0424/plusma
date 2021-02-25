9ca6da5e339b499496bc98a62bd95c53bd6ec9db
kanako <budoukan0424@gmail.com>
2021-02-26 01:37:01 +0900

    posts first commit

--- src/App.js
+++ src/App.js
@@ -19,11 +19,47 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-const helloWorld = <p>hello world!</p>
-function App() {
-  return helloWorld;
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app(); // if already initialized, use that one
 }
+
+function App() {
+  const [posts, setPosts] = useState([]);
+  const handleFetchButton = async () => {
+    const db = firebase.firestore();
+    const snapshot = await db
+      .collection('posts')
+      .get();
+    const _posts = [];
+    snapshot.forEach(doc => {
+      _posts.push({
+        postId: doc.id,
+        ...doc.data()
+      });
+    });
+    setPosts(_posts);
+  };
+
+  const postListItems = posts.map(post => {
+    return (
+      <li key={post.postId}>
+        {post.postName} : {post.price}: {post.imageUrl} 
+      </li>
+    );
+  });
+
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <button onClick={handleFetchButton}>取得</button>
+      <ul>{postListItems}</ul>
+    </div>
+  );
+}
+
+//this is a test for git pull origin main
+
 
 export default App;
