56dbcf81a0cb52cfba4fd082371e81aab27f8379
kanako <budoukan0424@gmail.com>
2021-03-03 21:55:34 +0900

    some file is added to create posts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-/*global App, _users*/
+
 /*eslint no-undef: "error"*/
 
 const firebaseConfig = {
@@ -26,40 +26,10 @@ if (!firebase.apps.length) {
 }
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const handleFetchButton = async () => {
-    const db = firebase.firestore();
-    const snapshot = await db
-      .collection('posts')
-      .get();
-    const _posts = [];
-    snapshot.forEach(doc => {
-      _posts.push({
-        postId: doc.id,
-        ...doc.data()
-      });
-    });
-    setPosts(_posts);
-  };
-
-  const postListItems = posts.map(post => {
-    return (
-      <li key={post.postId}>
-        {post.postName} : {post.price}: {post.imageUrl} 
-      </li>
-    );
-  });
-
+ 
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-      <button onClick={handleFetchButton}>取得</button>
-      <ul>{postListItems}</ul>
-    </div>
+    <div></div>
   );
 }
-
-//this is a test for git pull origin main
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { PostEdit, Posts} from './Posts.jsx';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Posts />
   </React.StrictMode>,
   document.getElementById('root')
 );
