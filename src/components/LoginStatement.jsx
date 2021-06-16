0214639633f93ecaa08013606730d7914ae64f71
kanako <budoukan0424@gmail.com>
2021-06-16 16:31:04 +0900

    solve loginstatement warning

--- src/components/LoginStatement.jsx
+++ src/components/LoginStatement.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import Header from './Header';
-import NavBar from './NavBar';
+// import Header from './Header';
+// import NavBar from './NavBar';
 
 function LoginStatement() {
   return (
